ac3e465c7901f80c8d4e719500a0d2f77133eeb5
Leandro Perez <lnpa2524@gmail.com>
2026-04-18 23:53:55 -0500

    Twitter Follow Card Terminada

---
+++ Projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+export function TwitterFollowCard ({formatUsername ,userName, name, initialIsFollowing}) {
+
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing 
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
+
+    
+
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
+    return(
+
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img 
+                className='tw-followCard-avatar' 
+                alt="AvatarLeandro" 
+                src={'https://unavatar.io/github/${userName}'} />
+                <div className='tw-followCard-info'>
+                    <strong >{name}</strong>
+                    <span 
+                    className='tw-followCard-infoUsername'>
+                        {formatUsername(userName)}
+                        </span>
+                </div>
+            </header>
+
+            <aside>
+                <button className={buttonClassName} onClick={handleClick}>
+                    {text}
+                </button>
+            </aside>
+        </article>
+    )
+
+}
